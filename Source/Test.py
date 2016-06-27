9d8f08e8a16c75e466d4558fbccbbccecd42415e
88konstantiny88 <smcda40@gmail.com>
2016-06-27 19:58:24 +0300

    test

--- Source/Test.py
+++ Source/Test.py
@@ -3,7 +3,9 @@ Created on 24 черв. 2016
 
 @author: koss
 '''
-    
+import string;
+import socket;
+
 #   tuple example 
 a = "hello";
 b = 5;
@@ -16,13 +18,33 @@ hh = (a,b,c);
 #         print(str(index) + " " + hh[index]);
 
 
+
+def getAddr(value):
+    addr = (socket.gethostbyname(value));
+    return addr;
+
+
+
 def printAllStrings(value):
     if(value == 5):
         for index,i in enumerate(hh):
             print(hh[index-1]);
     else:
-        print(hh);
+        return print(value);
+    
+
+def letterCount(text, letters = string.ascii_letters):
+    letters = frozenset(letters);
+    count = 0;
+    for char in text:
+        if char in letters:
+                count +=1 ;
+        return count;
+
     
     
-    
-printAllStrings(b);    
+printAllStrings(b);
+print(letterCount("Maggie and Hopey"));
+print(getAddr("google.ua"));
+print(getAddr("google.com"));
+print(getAddr("4pda.ru"));    
