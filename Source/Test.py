f6e5405ee3b17fd6eaad5512a34b3c5188dd26df
88konstantiny88 <smcda40@gmail.com>
2016-06-27 19:14:05 +0300

    //

--- Source/Test.py
+++ Source/Test.py
@@ -3,21 +3,26 @@ Created on 24 черв. 2016
 
 @author: koss
 '''
-
+    
 #   tuple example 
+a = "hello";
+b = 5;
+c = 123.67;
 
-hh = ("hello", "hello2", "hello3");
+hh = (a,b,c);
 
 # def printAllStrings():
 #     for index, i in enumerate(hh):
 #         print(str(index) + " " + hh[index]);
 
 
-def printAllStrings():
-    for i in enumerate(hh):
+def printAllStrings(value):
+    if(value == 5):
+        for index,i in enumerate(hh):
+            print(hh[index-1]);
+    else:
         print(hh);
     
     
     
-    
-printAllStrings();    
+printAllStrings(b);    
